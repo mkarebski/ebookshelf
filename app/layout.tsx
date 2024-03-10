216309e15299208185a4678ec943d3219649a614
Mikołaj Karebski <mikolaj.karebski@gmail.com>
2024-03-10 16:09:32 +0100

    Complete chapters 2 and 3 of the next.js tutorial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+      <html lang="en">
+      {/*<body>{children}</body>*/}
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
   );
 }
