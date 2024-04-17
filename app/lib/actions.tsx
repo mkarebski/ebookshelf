eb55614699e10568cf59a7289f31278656fd73df
Mikołaj Karebski <mikolaj.karebski@gmail.com>
2024-04-17 21:55:51 +0200

    Validate the data

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -1,11 +1,23 @@
 'use server'
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
-    // Test it out:
-    console.log(rawFormData);
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 }
