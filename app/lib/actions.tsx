877f88300e61e7ad291cab05ff0e00565ce600cf
Mikołaj Karebski <mikolaj.karebski@gmail.com>
2024-04-19 21:18:54 +0200

    Add insert to DB

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -20,4 +20,9 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 }
