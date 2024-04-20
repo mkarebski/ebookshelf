e6ea38f27ee84bb510037fa5f37702ec09373944
Mikołaj Karebski <mikolaj.karebski@gmail.com>
2024-04-20 18:08:16 +0200

    Add data refresh after saving it to DB (part 2)

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -3,6 +3,8 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 
 const FormSchema = z.object({
     id: z.string(),
@@ -29,4 +31,5 @@ export async function createInvoice(formData: FormData) {
   `;
 
     revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
